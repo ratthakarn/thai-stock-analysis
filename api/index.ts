03b6cdab9f319011dd4ca8b49ec5be3e95058103
ratthakarn <gkrgroup.co.th@gmail.com>
2026-06-24 21:27:54 +0700

    Debug: screener alone

--- api/index.ts
+++ api/index.ts
@@ -1,16 +1,11 @@
 import express from 'express'
-import * as yahoo from '../server/services/yahoo.js'
-import * as tradingview from '../server/services/tradingview.js'
-import * as filebase from '../server/services/filebase.js'
-import * as claude from '../server/services/claude.js'
-import * as indicators from '../server/services/indicators.js'
-import * as news from '../server/services/news.js'
+import * as screener from '../server/services/screener.js'
 
 const app = express()
 app.use(express.json())
 
 app.get('/api/debug', (_req, res) => {
-  res.json({ indicators: typeof indicators, news: typeof news })
+  res.json({ screener: typeof screener, keys: Object.keys(screener) })
 })
 
 export default app
